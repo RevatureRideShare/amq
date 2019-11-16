170406c9e2a0f9e6cb2a2d7cb1892b6a406b0aae
Erik Haklar <erikhaklar@gmail.com>
2019-11-15 18:10:02 -0600

    Blank Base Project Upload

---
+++ AMQMaster/src/main/java/com/example/amq/AmqMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.amq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqMasterApplication.class, args);
+	}
+
+}
---
+++ AMQMaster/src/test/java/com/example/amq/AmqMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
